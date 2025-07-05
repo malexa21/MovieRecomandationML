9159d6c3861cf1913f015692b05ad09c5758ac89
malexa21 <aaliroalexandra@gmail.com>
2025-07-05 16:23:16 +0300

    Added movie list and created interface coresponding with them

---
+++ src/MoviesDataBase.jsx
@@ -0,0 +1,144 @@
+const Movies =[
+    {
+    id: 1,
+    title: "Inception",
+    genres: ["Sci-Fi", "Thriller"],
+    year: 2010,
+    rating: 8.8
+  },
+  {
+    id: 2,
+    title: "The Shawshank Redemption",
+    genres: ["Drama"],
+    year: 1994,
+    rating: 9.3
+  },
+  {
+    id: 3,
+    title: "The Dark Knight",
+    genres: ["Action", "Crime", "Drama"],
+    year: 2008,
+    rating: 9.0
+  },
+  {
+    id: 4,
+    title: "Pulp Fiction",
+    genres: ["Crime", "Drama"],
+    year: 1994,
+    rating: 8.9
+  },
+  {
+    id: 5,
+    title: "Forrest Gump",
+    genres: ["Drama", "Romance"],
+    year: 1994,
+    rating: 8.8
+  },
+  {
+    id: 6,
+    title: "The Matrix",
+    genres: ["Sci-Fi", "Action"],
+    year: 1999,
+    rating: 8.7
+  },
+  {
+    id: 7,
+    title: "Parasite",
+    genres: ["Drama", "Thriller"],
+    year: 2019,
+    rating: 8.6
+  },
+  {
+    id: 8,
+    title: "Interstellar",
+    genres: ["Sci-Fi", "Adventure", "Drama"],
+    year: 2014,
+    rating: 8.6
+  },
+  {
+    id: 9,
+    title: "The Godfather",
+    genres: ["Crime", "Drama"],
+    year: 1972,
+    rating: 9.2
+  },
+  {
+    id: 10,
+    title: "Fight Club",
+    genres: ["Drama"],
+    year: 1999,
+    rating: 8.8
+  },
+  {
+    id: 11,
+    title: "Spirited Away",
+    genres: ["Animation", "Adventure", "Family"],
+    year: 2001,
+    rating: 8.6
+  },
+  {
+    id: 12,
+    title: "The Silence of the Lambs",
+    genres: ["Crime", "Thriller", "Horror"],
+    year: 1991,
+    rating: 8.6
+  },
+  {
+    id: 13,
+    title: "Gladiator",
+    genres: ["Action", "Adventure", "Drama"],
+    year: 2000,
+    rating: 8.5
+  },
+  {
+    id: 14,
+    title: "Whiplash",
+    genres: ["Drama", "Music"],
+    year: 2014,
+    rating: 8.5
+  },
+  {
+    id: 15,
+    title: "The Grand Budapest Hotel",
+    genres: ["Comedy", "Drama", "Adventure"],
+    year: 2014,
+    rating: 8.1
+  },
+  {
+    id: 16,
+    title: "Her",
+    genres: ["Drama", "Romance", "Sci-Fi"],
+    year: 2013,
+    rating: 8.0
+  },
+  {
+    id: 17,
+    title: "Mad Max: Fury Road",
+    genres: ["Action", "Adventure", "Sci-Fi"],
+    year: 2015,
+    rating: 8.1
+  },
+  {
+    id: 18,
+    title: "Get Out",
+    genres: ["Horror", "Mystery", "Thriller"],
+    year: 2017,
+    rating: 7.7
+  },
+  {
+    id: 19,
+    title: "La La Land",
+    genres: ["Comedy", "Drama", "Music"],
+    year: 2016,
+    rating: 8.0
+  },
+  {
+    id: 20,
+    title: "The Social Network",
+    genres: ["Biography", "Drama"],
+    year: 2010,
+    rating: 7.7
+  }
+];
+
+export default Movies;
